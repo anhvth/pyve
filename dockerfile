f5c0c4f819c4e1a2ca331ac785def7a439d50d0e
anh vo <anhvth.226@gmail.com>
2025-09-08 17:57:32 +0700

    Remove Dockerfile and installation script; delete test script for Docker image

--- dockerfile
+++
@@ -1,6 +0,0 @@
-FROM ubuntu:latest
-COPY install.sh /install.sh
-RUN chmod +x /install.sh && /install.sh
-ENV PATH="/root/.local/bin:$PATH"
-# Simple Ubuntu Docker image
-# You can add more instructions here as needed
--- test_docker.sh
+++
@@ -1,3 +0,0 @@
-# docker build -t my-ubuntu-image .
-cmd_to_run="uv run --with rich https://raw.githubusercontent.com/anhvth/vex/ecf3b5a700fccf36bcfa7f41a566217eaded6069/pyve/cli.py"
-docker run -it -v $(pwd):/workspace --workdir /workspace my-ubuntu-image /bin/sh -c "$cmd_to_run"
